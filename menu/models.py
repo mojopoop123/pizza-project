30e935b84a4b0ad61788ecef07aa10792b014a9d
sannesophietellefsen <sannetellefsen@gmail.com>
2026-06-04 17:50:14 +0200

    stuff

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class MenuItem(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='menu_images')
+    allergens = models.CharField(max_length=200)
+    active = models.BooleanField(default=True)
