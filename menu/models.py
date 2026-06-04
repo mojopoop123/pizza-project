2c9e5fe8eb1aba774ad40fbd311ed65a3e7a6ec7
sannesophietellefsen <sannetellefsen@gmail.com>
2026-06-04 19:55:30 +0200

    more changes

--- menu/models.py
+++ menu/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class MenuItem(models.Model):
+    class MenuItemType(models.TextChoices):
+        PIZZA = 'PIZZA', _('Pizza')
+        DRINK = 'DRINK', _('Drink')
     title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.IntegerField()
-    image = models.ImageField(upload_to='menu_images')
+    image_url = models.TextField()
     allergens = models.CharField(max_length=200)
     active = models.BooleanField(default=True)
+    type = models.CharField(choices=MenuItemType.choices, default=MenuItemType.PIZZA, max_length=5)
+
